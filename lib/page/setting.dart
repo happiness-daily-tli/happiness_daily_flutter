79851baaba7235229201d71dac668f76ab444fd1
SeongYongLee <seongyong.dev@gmail.com>
2021-09-01 18:36:42 +0900

    세팅 페이지 폼 작업 중

--- lib/page/setting.dart
+++ lib/page/setting.dart
@@ -9,21 +9,71 @@ class SettingPage extends ConsumerWidget {
     final user = ref.watch(userProvider);
 
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '닉네임을 입력해주세요',
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 52,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                border: Border.all(
+                  width: 1,
+                  color: Colors.black,
+                ),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15, right: 20),
+                    child: Text(
+                      '닉네임',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: '닉네임을 입력해주세요',
+                        hintStyle:
+                            TextStyle(fontSize: 14, color: Color(0xff999999)),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          Text('행복일기 시작하기'),
-        ],
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              height: 52,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                color: Color(0xff6b53ff),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+              child: Center(
+                child: Text('행복일기 시작하기',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
