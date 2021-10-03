c6fa88a631c80a1e51439c2a9647921cda7bd95a
SeongYongLee <seongyong.dev@gmail.com>
2021-10-04 01:34:35 +0900

    SingleChildScrollView, bottomNavigationBar가 동시에 표시되지 않는 오류 수정

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -10,36 +10,71 @@ class HomePage extends ConsumerWidget {
     final tabIndex = ref.watch(tabIndexProvider);
 
     return Scaffold(
-      appBar: AppBar(title: Text('행복일기')),
-      // TODO : Show List, Edit
-      body: Text('${user.state}님 안녕하세요.'),
-      bottomNavigationBar: SizedBox(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              height: 20.0,
+              color: Colors.red,
+            ),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST'),
+            Text('TEST')
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.only(top: 0, bottom: 0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
         height: 83,
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          selectedItemColor: Colors.black,
-          unselectedItemColor: Colors.black.withOpacity(.60),
-          selectedFontSize: 10,
-          unselectedFontSize: 10, //현재 선택된 Index
-          onTap: (int index) => tabIndex.state = index,
-          currentIndex: tabIndex.state,
-          items: [
-            BottomNavigationBarItem(
-              label: '나의행복',
-              icon: SvgPicture.asset('assets/tab_0.svg'),
-              activeIcon: SvgPicture.asset('assets/tab_0_active.svg'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: Colors.white,
+              selectedItemColor: Colors.black,
+              unselectedItemColor: Colors.black.withOpacity(.60),
+              selectedFontSize: 10,
+              unselectedFontSize: 10, //현재 선택된 Index
+              elevation: 0,
+              onTap: (int index) => tabIndex.state = index,
+              currentIndex: tabIndex.state,
+              items: [
+                BottomNavigationBarItem(
+                  label: '나의행복',
+                  icon: SvgPicture.asset('assets/tab_0.svg'),
+                  activeIcon: SvgPicture.asset('assets/tab_0_active.svg'),
+                ),
+                BottomNavigationBarItem(
+                  label: '행복찾기',
+                  icon: SvgPicture.asset('assets/tab_1.svg'),
+                  activeIcon: SvgPicture.asset('assets/tab_1_active.svg'),
+                ),
+                BottomNavigationBarItem(
+                  label: '우리의행복',
+                  icon: SvgPicture.asset('assets/tab_2.svg'),
+                  activeIcon: SvgPicture.asset('assets/tab_2_active.svg'),
+                ),
+              ],
             ),
-            BottomNavigationBarItem(
-              label: '행복찾기',
-              icon: SvgPicture.asset('assets/tab_1.svg'),
-              activeIcon: SvgPicture.asset('assets/tab_1_active.svg'),
-            ),
-            BottomNavigationBarItem(
-              label: '우리의행복',
-              icon: SvgPicture.asset('assets/tab_2.svg'),
-              activeIcon: SvgPicture.asset('assets/tab_2_active.svg'),
-            ),
+            Container(
+              height: 20,
+              color: Colors.white,
+            )
           ],
         ),
       ),
