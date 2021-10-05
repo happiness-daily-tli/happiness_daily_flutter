2d5d964755ad671f8ec9c0e69e343dd774013c40
SeongYongLee <seongyong.dev@gmail.com>
2021-10-06 01:45:56 +0900

    HomePage 작업 중

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:happiness_daily_flutter/component/common/Footer.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,29 +7,50 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+        child: Stack(
           children: <Widget>[
+            Container(height: 1500),
             Image.asset("assets/home/background.png"),
-            Container(
-              height: 100,
-              decoration: BoxDecoration(
+            Positioned(
+              top: 548,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                height: 1000,
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0)),
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(20.0)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '나의 행복기록',
+                          style: TextStyle(
+                            color: Color(0xFF1A1A1A),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        Spacer(),
+                        Text(
+                          '2021.07',
+                          style: TextStyle(
+                            color: Color(0xFF999999),
+                            fontWeight: FontWeight.w300,
+                            fontSize: 14,
+                          ),
+                        ),
+                        SvgPicture.asset('assets/home/more.svg'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST'),
-            Text('TEST')
           ],
         ),
       ),
