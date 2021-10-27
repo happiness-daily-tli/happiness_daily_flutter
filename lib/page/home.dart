7a6c2de2d8a23774b8cee5ab6d4f7aa284dc9e34
SeongYongLee <seongyong.dev@gmail.com>
2021-10-28 02:19:21 +0900

    BottomNavigationBar with Navigator

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,58 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:happiness_daily_flutter/component/common/Footer.dart';
+import 'package:happiness_daily_flutter/state/index.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabIndex = ref.watch(tabIndexProvider);
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          children: <Widget>[
-            Container(height: 1500),
-            Image.asset("assets/home/background.png"),
-            Positioned(
-              top: 548,
-              width: MediaQuery.of(context).size.width,
-              child: Container(
-                height: 1000,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.vertical(top: Radius.circular(20.0)),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          '나의 행복기록',
-                          style: TextStyle(
-                            color: Color(0xFF1A1A1A),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                          ),
+      body: new Stack(
+        children: [
+          Offstage(
+            offstage: tabIndex.state != 0,
+            child: TickerMode(
+              enabled: tabIndex.state == 0,
+              child: SingleChildScrollView(
+                child: Stack(
+                  children: <Widget>[
+                    Container(height: 1500),
+                    Image.asset("assets/home/background.png"),
+                    Positioned(
+                      top: 548,
+                      width: MediaQuery.of(context).size.width,
+                      child: Container(
+                        height: 1000,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius:
+                              BorderRadius.vertical(top: Radius.circular(20.0)),
                         ),
-                        Spacer(),
-                        Text(
-                          '2021.07',
-                          style: TextStyle(
-                            color: Color(0xFF999999),
-                            fontWeight: FontWeight.w300,
-                            fontSize: 14,
-                          ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  '나의 행복기록',
+                                  style: TextStyle(
+                                    color: Color(0xFF1A1A1A),
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                                Spacer(),
+                                Text(
+                                  '2021.07',
+                                  style: TextStyle(
+                                    color: Color(0xFF999999),
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                SvgPicture.asset('assets/home/more.svg'),
+                              ],
+                            ),
+                          ],
                         ),
-                        SvgPicture.asset('assets/home/more.svg'),
-                      ],
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+          Offstage(
+            offstage: tabIndex.state != 1,
+            child: TickerMode(
+              enabled: tabIndex.state == 1,
+              child: Center(child: Text('1')),
+            ),
+          ),
+          Offstage(
+            offstage: tabIndex.state != 2,
+            child: TickerMode(
+              enabled: tabIndex.state == 2,
+              child: Center(child: Text('2')),
+            ),
+          ),
+        ],
       ),
       bottomNavigationBar: Footer(),
     );
