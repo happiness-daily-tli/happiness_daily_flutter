71f1a8d592434e432195a1e564ae3fc60d74f23d
SeongYongLee <seongyong.dev@gmail.com>
2021-12-14 16:17:28 +0900

    feat: flutter version update, splash image change

--- lib/components/home/my_happiness/record_list_header.dart
+++ lib/components/home/my_happiness/record_list_header.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class RecordListHeader extends ConsumerWidget {
