301c75b0abde8eae7eb364bc8607b6227bf058a9
SeongYongLee <seongyong.dev@gmail.com>
2022-01-15 16:49:50 +0900

    feat: lottie 추가, my_happiness/music 작업

--- lib/components/my_happiness/music.dart
+++ lib/components/my_happiness/music.dart
@@ -1,23 +1,52 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Music extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Music extends StatefulWidget {
+  @override
+  State<Music> createState() => _MusicState();
+}
+
+class _MusicState extends State<Music> {
+  bool isShow = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 3), () {
+      setState(() {
+        isShow = !isShow;
+        print(isShow);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset(
-          'assets/images/common/icon/music.png',
-          width: 14,
-          height: 14,
+        Lottie.asset(
+          'assets/lottie/bgm.json',
+          width: 25,
+          height: 25,
         ),
-        SizedBox(height: 10),
-        Text(
-          '음악을 들으며',
-          style: TextStyle(color: Colors.white),
-        ),
-        Text(
-          '오늘의 행복을 떠올려 보세요',
-          style: TextStyle(color: Colors.white),
+        AnimatedOpacity(
+          opacity: isShow ? 0.0 : 1.0,
+          duration: Duration(seconds: 1),
+          child: Column(
+            children: [
+              Text(
+                '음악을 들으며',
+                style: TextStyle(color: Colors.white),
+              ),
+              Text(
+                '오늘의 행복을 떠올려 보세요',
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ],
     );
--- lib/pages/my_happiness.dart
+++ lib/pages/my_happiness.dart
@@ -17,62 +17,67 @@ class MyHappinessPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     AsyncValue<List<Record>> record = ref.watch(recordProvider);
 
-    return ListView(
-      padding: EdgeInsets.zero,
-      primary: true,
-      children: [
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              height: 640,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/home/background.png"),
-                  fit: BoxFit.fitHeight,
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        primary: true,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                height: 640,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/home/background.png"),
+                    fit: BoxFit.fitHeight,
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 60,
-              left: 10,
-              child: IconButton(
-                icon: Image.asset('assets/images/common/icon/hamburger.png'),
-                iconSize: 32,
-                onPressed: () => handleDrawer(),
+              Positioned(
+                top: 60,
+                left: 10,
+                child: IconButton(
+                  icon: Image.asset('assets/images/common/icon/hamburger.png'),
+                  iconSize: 32,
+                  onPressed: () => handleDrawer(),
+                ),
               ),
-            ),
-            Positioned(
-              top: 90,
-              child: Music(),
-            ),
-            Positioned(
-              top: 190,
-              right: 20,
-              child: SpeechBubble(),
-            ),
-            Positioned(
-              top: 438,
-              width: MediaQuery.of(context).size.width,
-              child: Level(),
-            ),
-            Positioned(
-              top: 528,
-              width: MediaQuery.of(context).size.width,
-              child: RecordListHeader(),
-            ),
-          ],
-        ),
-        Center(
-          child: record.when(
-            loading: () => const CircularProgressIndicator(),
-            error: (err, stack) => Text('Error: $err'),
-            data: (record) {
-              return RecordListView(recordList: record);
-            },
+              Positioned(
+                top: 90,
+                child: Music(),
+              ),
+              Positioned(
+                top: 190,
+                right: 20,
+                child: SpeechBubble(),
+              ),
+              Positioned(
+                top: 438,
+                width: MediaQuery.of(context).size.width,
+                child: Level(),
+              ),
+              Positioned(
+                top: 528,
+                width: MediaQuery.of(context).size.width,
+                child: RecordListHeader(),
+              ),
+            ],
           ),
-        ),
-      ],
+          Center(
+            child: record.when(
+              loading: () => const CircularProgressIndicator(),
+              error: (err, stack) => Text('Error: $err'),
+              data: (record) {
+                return RecordListView(recordList: record);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
