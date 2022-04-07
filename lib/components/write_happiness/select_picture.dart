f09b89840b4ec8e6b11d944b08e49167f76b6c4e
SeongYongLee <seongyong.dev@gmail.com>
2022-04-08 00:03:11 +0900

    feat: SelectPicture에 ImagePicker 추가

--- lib/components/write_happiness/select_picture.dart
+++ lib/components/write_happiness/select_picture.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:happiness_daily_flutter/components/common/alert_dialog_widget.dart';
 import 'package:happiness_daily_flutter/constants/index.dart';
+import 'package:happiness_daily_flutter/happiness_theme.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SelectPicture extends StatefulWidget {
   @override
@@ -8,9 +10,26 @@ class SelectPicture extends StatefulWidget {
 }
 
 class _SelectPictureState extends State<SelectPicture> {
+  final ImagePicker _picker = ImagePicker();
+  List<String> images = [];
+
   @override
   void initState() {
     super.initState();
+  }
+
+  Future<void> _pickImg(bool isCamera) async {
+    final XFile? image = await _picker.pickImage(
+      source: isCamera ? ImageSource.camera : ImageSource.gallery,
+    );
+
+    if (image != null) {
+      setState(() {
+        images.add(image.path);
+      });
+    }
+
+    Navigator.pop(context);
   }
 
   @override
@@ -28,14 +47,23 @@ class _SelectPictureState extends State<SelectPicture> {
             headerText: '사진을 선택해주세요.',
             contentWidget: Column(
               children: [
-                Container(
-                  height: 60,
+                TextButton(
+                  onPressed: () => _pickImg(false),
                   child: Padding(
-                    padding: const EdgeInsets.all(20.0),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15.0,
+                      vertical: 10.0,
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('갤러리'),
+                        Text(
+                          '갤러리',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: black,
+                          ),
+                        ),
                         Image.asset(
                           'assets/images/common/icon/gallery.png',
                           height: 30,
@@ -53,14 +81,23 @@ class _SelectPictureState extends State<SelectPicture> {
                     ),
                   ),
                 ),
-                Container(
-                  height: 60,
+                TextButton(
+                  onPressed: () => _pickImg(true),
                   child: Padding(
-                    padding: const EdgeInsets.all(20.0),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 15.0,
+                      vertical: 10.0,
+                    ),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('카메라'),
+                        Text(
+                          '카메라',
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: black,
+                          ),
+                        ),
                         Image.asset(
                           'assets/images/common/icon/camera.png',
                           height: 30,
@@ -79,25 +116,42 @@ class _SelectPictureState extends State<SelectPicture> {
 
     return GestureDetector(
       onTap: () => _openSelectPictureTypeDialog(),
-      child: Container(
-        width: 60,
-        height: 60,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(
-            Radius.circular(10.0),
+      child: Row(
+        children: [
+          Row(
+            children: images.map(
+              (image) {
+                return Container(
+                  width: 60,
+                  height: 60,
+                  child: Image.asset(
+                    image,
+                  ),
+                );
+              },
+            ).toList(),
           ),
-          border: Border.all(
-            width: 1,
-            color: Theme.of(context).colorScheme.primary,
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10.0),
+              ),
+              border: Border.all(
+                width: 1,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            child: Center(
+              child: Image.asset(
+                'assets/images/common/icon/camera.png',
+                width: 30,
+                height: 30,
+              ),
+            ),
           ),
-        ),
-        child: Center(
-          child: Image.asset(
-            'assets/images/common/icon/camera.png',
-            width: 30,
-            height: 30,
-          ),
-        ),
+        ],
       ),
     );
   }
